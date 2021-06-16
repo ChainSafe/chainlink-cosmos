00871e2b29d184a74b0d79daeca97033b05f8209
araskachoi <choidanielw@gmail.com>
2021-06-16 08:55:25 -0700

    edit proto files

--- x/chainlink/types/query.pb.go
+++ x/chainlink/types/query.pb.go
@@ -495,10 +495,7 @@ func (m *QueryFeedDataRequest) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthQuery
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthQuery
 			}
 			if (iNdEx + skippy) > l {
@@ -618,10 +615,7 @@ func (m *QueryFeedDataResponse) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthQuery
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthQuery
 			}
 			if (iNdEx + skippy) > l {
--- x/chainlink/types/tx.pb.go
+++ x/chainlink/types/tx.pb.go
@@ -29,8 +29,8 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type MsgFeedData struct {
 	FeedId    string                                        `protobuf:"bytes,1,opt,name=feedId,proto3" json:"feedId,omitempty"`
-	Submitter github_com_cosmos_cosmos_sdk_types.AccAddress `protobuf:"bytes,2,opt,name=submitter,proto3,casttype=github.com/cosmos/cosmos-sdk/types.AccAddress" json:"submitter,omitempty"`
-	FeedData  string                                        `protobuf:"bytes,3,opt,name=feedData,proto3" json:"feedData,omitempty"`
+	FeedOwner github_com_cosmos_cosmos_sdk_types.AccAddress `protobuf:"bytes,2,opt,name=feedOwner,proto3,casttype=github.com/cosmos/cosmos-sdk/types.AccAddress" json:"feedOwner,omitempty"`
+	FeedData  *OCRAbiEncoded                                `protobuf:"bytes,3,opt,name=feedData,proto3" json:"feedData,omitempty"`
 }
 
 func (m *MsgFeedData) Reset()         { *m = MsgFeedData{} }
@@ -73,44 +73,128 @@ func (m *MsgFeedData) GetFeedId() string {
 	return ""
 }
 
-func (m *MsgFeedData) GetSubmitter() github_com_cosmos_cosmos_sdk_types.AccAddress {
+func (m *MsgFeedData) GetFeedOwner() github_com_cosmos_cosmos_sdk_types.AccAddress {
+	if m != nil {
+		return m.FeedOwner
+	}
+	return nil
+}
+
+func (m *MsgFeedData) GetFeedData() *OCRAbiEncoded {
+	if m != nil {
+		return m.FeedData
+	}
+	return nil
+}
+
+// this will be the implementation used later will use pseudo OCR ABI encoded data instead
+// because the structure of how the OCR will be generalized is still unknown
+// OCRAbiEncoded implments the OCR data that is ABCI encoded. The use and form will conform to the
+// Chainlink protocol specification.
+type OCRAbiEncoded struct {
+	// Context should be a 32-byte array struct.
+	Context []byte `protobuf:"bytes,1,opt,name=Context,proto3" json:"Context,omitempty"`
+	// Oracles should be a 32-byte record of all participating oracles.
+	Oracles []byte `protobuf:"bytes,2,opt,name=Oracles,proto3" json:"Oracles,omitempty"`
+	// Observations should be an array on int192 containing the proviers' independent observations.
+	Observations [][]byte `protobuf:"bytes,3,rep,name=Observations,proto3" json:"Observations,omitempty"`
+	// Submitter is the Chainlink node address that has submitted the price feed data.
+	// The Submitter will be paid out the reward that has been set by the Feed Owner.
+	Submitter []byte `protobuf:"bytes,4,opt,name=Submitter,proto3" json:"Submitter,omitempty"`
+}
+
+func (m *OCRAbiEncoded) Reset()         { *m = OCRAbiEncoded{} }
+func (m *OCRAbiEncoded) String() string { return proto.CompactTextString(m) }
+func (*OCRAbiEncoded) ProtoMessage()    {}
+func (*OCRAbiEncoded) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8e2cf97733d10959, []int{1}
+}
+func (m *OCRAbiEncoded) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *OCRAbiEncoded) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_OCRAbiEncoded.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *OCRAbiEncoded) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_OCRAbiEncoded.Merge(m, src)
+}
+func (m *OCRAbiEncoded) XXX_Size() int {
+	return m.Size()
+}
+func (m *OCRAbiEncoded) XXX_DiscardUnknown() {
+	xxx_messageInfo_OCRAbiEncoded.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_OCRAbiEncoded proto.InternalMessageInfo
+
+func (m *OCRAbiEncoded) GetContext() []byte {
+	if m != nil {
+		return m.Context
+	}
+	return nil
+}
+
+func (m *OCRAbiEncoded) GetOracles() []byte {
+	if m != nil {
+		return m.Oracles
+	}
+	return nil
+}
+
+func (m *OCRAbiEncoded) GetObservations() [][]byte {
+	if m != nil {
+		return m.Observations
+	}
+	return nil
+}
+
+func (m *OCRAbiEncoded) GetSubmitter() []byte {
 	if m != nil {
 		return m.Submitter
 	}
 	return nil
 }
 
-func (m *MsgFeedData) GetFeedData() string {
-	if m != nil {
-		return m.FeedData
-	}
-	return ""
-}
-
 func init() {
 	proto.RegisterType((*MsgFeedData)(nil), "chainlink.v1beta.MsgFeedData")
+	proto.RegisterType((*OCRAbiEncoded)(nil), "chainlink.v1beta.OCRAbiEncoded")
 }
 
 func init() { proto.RegisterFile("chainlink/v1beta/tx.proto", fileDescriptor_8e2cf97733d10959) }
 
 var fileDescriptor_8e2cf97733d10959 = []byte{
-	// 249 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x4c, 0xce, 0x48, 0xcc,
-	0xcc, 0xcb, 0xc9, 0xcc, 0xcb, 0xd6, 0x2f, 0x33, 0x4c, 0x4a, 0x2d, 0x49, 0xd4, 0x2f, 0xa9, 0xd0,
-	0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x80, 0x4b, 0xe9, 0x41, 0xa4, 0xa4, 0x44, 0xd2, 0xf3,
-	0xd3, 0xf3, 0xc1, 0x92, 0xfa, 0x20, 0x16, 0x44, 0x9d, 0xd2, 0x24, 0x46, 0x2e, 0x6e, 0xdf, 0xe2,
-	0x74, 0xb7, 0xd4, 0xd4, 0x14, 0x97, 0xc4, 0x92, 0x44, 0x21, 0x31, 0x2e, 0xb6, 0xb4, 0xd4, 0xd4,
-	0x14, 0xcf, 0x14, 0x09, 0x46, 0x05, 0x46, 0x0d, 0xce, 0x20, 0x28, 0x4f, 0xc8, 0x9f, 0x8b, 0xb3,
-	0xb8, 0x34, 0x29, 0x37, 0xb3, 0xa4, 0x24, 0xb5, 0x48, 0x82, 0x49, 0x81, 0x51, 0x83, 0xc7, 0xc9,
-	0xf0, 0xd7, 0x3d, 0x79, 0xdd, 0xf4, 0xcc, 0x92, 0x8c, 0xd2, 0x24, 0xbd, 0xe4, 0xfc, 0x5c, 0xfd,
-	0xe4, 0xfc, 0xe2, 0xdc, 0xfc, 0x62, 0x28, 0xa5, 0x5b, 0x9c, 0x92, 0xad, 0x5f, 0x52, 0x59, 0x90,
-	0x5a, 0xac, 0xe7, 0x98, 0x9c, 0xec, 0x98, 0x92, 0x52, 0x94, 0x5a, 0x5c, 0x1c, 0x84, 0x30, 0x43,
-	0x48, 0x8a, 0x8b, 0x23, 0x0d, 0x6a, 0xa9, 0x04, 0x33, 0xd8, 0x2a, 0x38, 0xdf, 0x88, 0x95, 0x8b,
-	0xd9, 0xb7, 0x38, 0xdd, 0x29, 0xf0, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c,
-	0x92, 0x63, 0x9c, 0xf0, 0x58, 0x8e, 0xe1, 0xc2, 0x63, 0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0xa2,
-	0xcc, 0x91, 0xac, 0x75, 0x06, 0x79, 0x34, 0x38, 0x31, 0x2d, 0x55, 0x1f, 0xee, 0x65, 0x5d, 0xa8,
-	0x53, 0x2a, 0x10, 0x42, 0x10, 0xb7, 0x24, 0xb1, 0x81, 0x7d, 0x6d, 0x0c, 0x08, 0x00, 0x00, 0xff,
-	0xff, 0x53, 0xb1, 0x30, 0xc2, 0x3a, 0x01, 0x00, 0x00,
+	// 349 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x51, 0xcd, 0x4e, 0xf2, 0x40,
+	0x14, 0x65, 0xbe, 0x7e, 0x1f, 0x9f, 0x0c, 0x35, 0x31, 0x8d, 0x31, 0xd5, 0x98, 0xd2, 0xb0, 0xea,
+	0x86, 0x36, 0xe0, 0xc2, 0x85, 0x2b, 0x40, 0x4d, 0x5c, 0x90, 0xc6, 0x61, 0xe7, 0x6e, 0x3a, 0x73,
+	0x29, 0x0d, 0xd0, 0x21, 0x9d, 0x01, 0xf1, 0x01, 0xdc, 0xfb, 0x2e, 0xbe, 0x84, 0x4b, 0x96, 0xae,
+	0x8c, 0x81, 0xb7, 0x70, 0x65, 0xfa, 0x03, 0x88, 0xab, 0xb9, 0xe7, 0x9c, 0xb9, 0x33, 0xe7, 0x9e,
+	0x8b, 0x4f, 0xd9, 0x90, 0x46, 0xf1, 0x38, 0x8a, 0x47, 0xde, 0xbc, 0x19, 0x80, 0xa2, 0x9e, 0x5a,
+	0xb8, 0xd3, 0x44, 0x28, 0x61, 0x1c, 0x6d, 0x25, 0x37, 0x97, 0xce, 0x8e, 0x43, 0x11, 0x8a, 0x4c,
+	0xf4, 0xd2, 0x2a, 0xbf, 0x57, 0x7f, 0x45, 0xb8, 0xda, 0x93, 0xe1, 0x2d, 0x00, 0xbf, 0xa6, 0x8a,
+	0x1a, 0x27, 0xb8, 0x3c, 0x00, 0xe0, 0x77, 0xdc, 0x44, 0x36, 0x72, 0x2a, 0xa4, 0x40, 0x86, 0x8f,
+	0x2b, 0x69, 0xe5, 0x3f, 0xc6, 0x90, 0x98, 0x7f, 0x6c, 0xe4, 0xe8, 0x9d, 0xe6, 0xd7, 0x47, 0xad,
+	0x11, 0x46, 0x6a, 0x38, 0x0b, 0x5c, 0x26, 0x26, 0x1e, 0x13, 0x72, 0x22, 0x64, 0x71, 0x34, 0x24,
+	0x1f, 0x79, 0xea, 0x69, 0x0a, 0xd2, 0x6d, 0x33, 0xd6, 0xe6, 0x3c, 0x01, 0x29, 0xc9, 0xee, 0x0d,
+	0xe3, 0x0a, 0x1f, 0x0c, 0x8a, 0x4f, 0x4d, 0xcd, 0x46, 0x4e, 0xb5, 0x55, 0x73, 0x7f, 0x7b, 0x76,
+	0xfd, 0x2e, 0x69, 0x07, 0xd1, 0x4d, 0xcc, 0x04, 0x07, 0x4e, 0xb6, 0x0d, 0xf5, 0x67, 0x84, 0x0f,
+	0xf7, 0x34, 0xc3, 0xc4, 0xff, 0xbb, 0x22, 0x56, 0xb0, 0x50, 0x99, 0x71, 0x9d, 0x6c, 0x60, 0xaa,
+	0xf8, 0x09, 0x65, 0x63, 0x90, 0xb9, 0x6f, 0xb2, 0x81, 0x46, 0x1d, 0xeb, 0x7e, 0x20, 0x21, 0x99,
+	0x53, 0x15, 0x89, 0x58, 0x9a, 0x9a, 0xad, 0x39, 0x3a, 0xd9, 0xe3, 0x8c, 0x73, 0x5c, 0xe9, 0xcf,
+	0x82, 0x49, 0xa4, 0x14, 0x24, 0xe6, 0xdf, 0xac, 0x7f, 0x47, 0xb4, 0xfe, 0x61, 0xad, 0x27, 0xc3,
+	0xce, 0xfd, 0xdb, 0xca, 0x42, 0xcb, 0x95, 0x85, 0x3e, 0x57, 0x16, 0x7a, 0x59, 0x5b, 0xa5, 0xe5,
+	0xda, 0x2a, 0xbd, 0xaf, 0xad, 0xd2, 0xc3, 0xe5, 0x8f, 0x7c, 0xba, 0xe9, 0x74, 0x7d, 0x3a, 0x00,
+	0x6f, 0x3b, 0x67, 0xa3, 0xc8, 0x6c, 0xb1, 0xa3, 0xf2, 0xd0, 0x82, 0x72, 0xb6, 0x9e, 0x8b, 0xef,
+	0x00, 0x00, 0x00, 0xff, 0xff, 0x24, 0x27, 0x34, 0x0b, 0xe3, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -175,17 +259,22 @@ func (m *MsgFeedData) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if len(m.FeedData) > 0 {
-		i -= len(m.FeedData)
-		copy(dAtA[i:], m.FeedData)
-		i = encodeVarintTx(dAtA, i, uint64(len(m.FeedData)))
+	if m.FeedData != nil {
+		{
+			size, err := m.FeedData.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintTx(dAtA, i, uint64(size))
+		}
 		i--
 		dAtA[i] = 0x1a
 	}
-	if len(m.Submitter) > 0 {
-		i -= len(m.Submitter)
-		copy(dAtA[i:], m.Submitter)
-		i = encodeVarintTx(dAtA, i, uint64(len(m.Submitter)))
+	if len(m.FeedOwner) > 0 {
+		i -= len(m.FeedOwner)
+		copy(dAtA[i:], m.FeedOwner)
+		i = encodeVarintTx(dAtA, i, uint64(len(m.FeedOwner)))
 		i--
 		dAtA[i] = 0x12
 	}
@@ -193,6 +282,59 @@ func (m *MsgFeedData) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i -= len(m.FeedId)
 		copy(dAtA[i:], m.FeedId)
 		i = encodeVarintTx(dAtA, i, uint64(len(m.FeedId)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *OCRAbiEncoded) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *OCRAbiEncoded) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *OCRAbiEncoded) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Submitter) > 0 {
+		i -= len(m.Submitter)
+		copy(dAtA[i:], m.Submitter)
+		i = encodeVarintTx(dAtA, i, uint64(len(m.Submitter)))
+		i--
+		dAtA[i] = 0x22
+	}
+	if len(m.Observations) > 0 {
+		for iNdEx := len(m.Observations) - 1; iNdEx >= 0; iNdEx-- {
+			i -= len(m.Observations[iNdEx])
+			copy(dAtA[i:], m.Observations[iNdEx])
+			i = encodeVarintTx(dAtA, i, uint64(len(m.Observations[iNdEx])))
+			i--
+			dAtA[i] = 0x1a
+		}
+	}
+	if len(m.Oracles) > 0 {
+		i -= len(m.Oracles)
+		copy(dAtA[i:], m.Oracles)
+		i = encodeVarintTx(dAtA, i, uint64(len(m.Oracles)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.Context) > 0 {
+		i -= len(m.Context)
+		copy(dAtA[i:], m.Context)
+		i = encodeVarintTx(dAtA, i, uint64(len(m.Context)))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -220,11 +362,38 @@ func (m *MsgFeedData) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovTx(uint64(l))
 	}
-	l = len(m.Submitter)
+	l = len(m.FeedOwner)
 	if l > 0 {
 		n += 1 + l + sovTx(uint64(l))
 	}
-	l = len(m.FeedData)
+	if m.FeedData != nil {
+		l = m.FeedData.Size()
+		n += 1 + l + sovTx(uint64(l))
+	}
+	return n
+}
+
+func (m *OCRAbiEncoded) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Context)
+	if l > 0 {
+		n += 1 + l + sovTx(uint64(l))
+	}
+	l = len(m.Oracles)
+	if l > 0 {
+		n += 1 + l + sovTx(uint64(l))
+	}
+	if len(m.Observations) > 0 {
+		for _, b := range m.Observations {
+			l = len(b)
+			n += 1 + l + sovTx(uint64(l))
+		}
+	}
+	l = len(m.Submitter)
 	if l > 0 {
 		n += 1 + l + sovTx(uint64(l))
 	}
@@ -300,6 +469,226 @@ func (m *MsgFeedData) Unmarshal(dAtA []byte) error {
 			iNdEx = postIndex
 		case 2:
 			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field FeedOwner", wireType)
+			}
+			var byteLen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				byteLen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if byteLen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + byteLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.FeedOwner = append(m.FeedOwner[:0], dAtA[iNdEx:postIndex]...)
+			if m.FeedOwner == nil {
+				m.FeedOwner = []byte{}
+			}
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field FeedData", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.FeedData == nil {
+				m.FeedData = &OCRAbiEncoded{}
+			}
+			if err := m.FeedData.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *OCRAbiEncoded) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: OCRAbiEncoded: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: OCRAbiEncoded: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Context", wireType)
+			}
+			var byteLen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				byteLen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if byteLen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + byteLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Context = append(m.Context[:0], dAtA[iNdEx:postIndex]...)
+			if m.Context == nil {
+				m.Context = []byte{}
+			}
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Oracles", wireType)
+			}
+			var byteLen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				byteLen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if byteLen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + byteLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Oracles = append(m.Oracles[:0], dAtA[iNdEx:postIndex]...)
+			if m.Oracles == nil {
+				m.Oracles = []byte{}
+			}
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Observations", wireType)
+			}
+			var byteLen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				byteLen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if byteLen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + byteLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Observations = append(m.Observations, make([]byte, postIndex-iNdEx))
+			copy(m.Observations[len(m.Observations)-1], dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 4:
+			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Submitter", wireType)
 			}
 			var byteLen int
@@ -332,48 +721,13 @@ func (m *MsgFeedData) Unmarshal(dAtA []byte) error {
 				m.Submitter = []byte{}
 			}
 			iNdEx = postIndex
-		case 3:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field FeedData", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowTx
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthTx
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthTx
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.FeedData = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipTx(dAtA[iNdEx:])
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthTx
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthTx
 			}
 			if (iNdEx + skippy) > l {
