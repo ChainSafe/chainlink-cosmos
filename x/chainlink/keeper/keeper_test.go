7051b107cf6c983b0ba825107d4bbabb197e9ffd
Yann Guineau <yann@chainsafe.io>
2021-07-07 13:38:38 -0400

    Improve feed key format test

--- x/chainlink/keeper/keeper_test.go
+++ x/chainlink/keeper/keeper_test.go
@@ -38,55 +38,41 @@ func setupKeeper(t testing.TB) (*Keeper, sdk.Context) {
 	return keeper, ctx
 }
 
-// func setupKeeper(t testing.TB) (*Keeper, sdk.Context) {
-// 	storeKey := sdk.NewKVStoreKey(types.StoreKey)
-// 	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
-
-// 	db := tmdb.NewMemDB()
-// 	stateStore := store.NewCommitMultiStore(db)
-// 	stateStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
-// 	stateStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
-// 	require.NoError(t, stateStore.LoadLatestVersion())
-
-// 	registry := codectypes.NewInterfaceRegistry()
-// 	keeper := NewKeeper(codec.NewProtoCodec(registry), storeKey, memStoreKey)
-
-// 	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
-// 	return keeper, ctx
-// }
-
 func TestFeedKeyStructure(t *testing.T) {
 	k, ctx := setupKeeper(t)
 	roundStore := ctx.KVStore(k.roundStoreKey)
 	feedStore := ctx.KVStore(k.feedDataStoreKey)
 
-	var tests = []struct {
-		feedId  string
-		roundId uint64
+	testCases := []struct {
+		feedId   string
+		roundIds []uint64
 	}{
-		{"test1", 1111},
-		{"test11", 111},
-		{"test111", 11},
-		{"test1111", 1},
+		{feedId: "test1", roundIds: []uint64{1, 11, 111, 1111}},
+		{feedId: "test11", roundIds: []uint64{1, 11, 111, 1111}},
+		{feedId: "test111", roundIds: []uint64{1, 11, 111, 1111}},
+		{feedId: "test1111", roundIds: []uint64{1, 11, 111, 1111}},
 	}
 
 	// Add all feed cases to store
-	for _, tt := range tests {
-		// force set roundId-1 for SetFeedData
-		roundStore.Set(types.KeyPrefix(types.RoundIdKey+"/"+tt.feedId), i64tob(tt.roundId-1))
+	for _, tc := range testCases {
+		for _, roundId := range tc.roundIds {
+			// force set roundId-1 for SetFeedData
+			roundStore.Set(types.KeyPrefix(types.RoundIdKey+"/"+tc.feedId), i64tob(roundId-1))
 
-		feedData := types.MsgFeedData{
-			FeedId: tt.feedId,
+			feedData := types.MsgFeedData{
+				FeedId:    tc.feedId,
+				Submitter: []byte(fmt.Sprintf("%s/%d", tc.feedId, roundId)),
+			}
+
+			k.SetFeedData(ctx, &feedData)
 		}
-
-		k.SetFeedData(ctx, &feedData)
 	}
 
 	// Retrieve key
-	for _, tt := range tests {
-		testName := fmt.Sprintf("%s,%d", tt.feedId, tt.roundId)
+	for _, tc := range testCases {
+		testName := fmt.Sprintf("%s,%v", tc.feedId, tc.roundIds)
 		t.Run(testName, func(t *testing.T) {
-			prefixKey := types.FeedDataKey + "/" + tt.feedId + "/"
+			prefixKey := types.FeedDataKey + "/" + tc.feedId + "/"
 			//fmt.Println("[DEBUG] search for key", prefixKey)
 
 			iterator := sdk.KVStorePrefixIterator(feedStore, types.KeyPrefix(prefixKey))
@@ -98,13 +84,9 @@ func TestFeedKeyStructure(t *testing.T) {
 				k.cdc.MustUnmarshalBinaryBare(iterator.Value(), &feedData)
 				//fmt.Println("[DEBUG] found key", string(iterator.Key()), feedData.FeedData.FeedId, feedData.RoundId)
 
-				if feedData.FeedData.FeedId != tt.feedId {
-					t.Errorf("FeedId: got %s, want %s", feedData.FeedData.FeedId, tt.feedId)
-				}
-
-				if feedData.RoundId != tt.roundId {
-					t.Errorf("RoundId: got %d, want %d", feedData.RoundId, tt.roundId)
-				}
+				require.Equal(t, tc.feedId, feedData.GetFeedData().GetFeedId())
+				require.Equal(t, []byte(fmt.Sprintf("%s/%d", tc.feedId, feedData.GetRoundId())), feedData.GetFeedData().GetSubmitter().Bytes())
+				require.Contains(t, tc.roundIds, feedData.GetRoundId())
 			}
 		})
 	}
