be1800f11984ec3957778d7fdce14527df533df3
araskachoi <choidanielw@gmail.com>
2021-07-14 14:26:45 -0700

    change token to link

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,9 +12,9 @@ chainlinkd keys add cerlo --keyring-backend test
 chainlinkd init testchain --chain-id testchain
 
 # Add 3 accounts, with coins to the genesis file
-chainlinkd add-genesis-account $(chainlinkd keys show alice -a --keyring-backend test) 1000token,100000000stake
-chainlinkd add-genesis-account $(chainlinkd keys show bob -a --keyring-backend test) 1000token,100000000stake
-chainlinkd add-genesis-account $(chainlinkd keys show cerlo -a --keyring-backend test) 1000token,100000000stake
+chainlinkd add-genesis-account $(chainlinkd keys show alice -a --keyring-backend test) 1000000link,100000000stake
+chainlinkd add-genesis-account $(chainlinkd keys show bob -a --keyring-backend test) 1000000link,100000000stake
+chainlinkd add-genesis-account $(chainlinkd keys show cerlo -a --keyring-backend test) 1000000link,100000000stake
 
 # Add init chainLink module owner to the genesis file
 chainlinkd tx chainlink add-genesis-module-owner $(chainlinkd keys show alice -a --keyring-backend test) $(chainlinkd keys show alice -p --keyring-backend test) --keyring-backend test --chain-id testchain
