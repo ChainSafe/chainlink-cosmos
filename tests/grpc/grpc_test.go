773174547a1a2a4ebfc8b9086976046e5504509c
Yann Guineau <yann@chainsafe.io>
2021-08-05 16:19:08 -0400

    Improve tests using test suite + generate genesis account/balance

--- tests/grpc/grpc_test.go
+++ tests/grpc/grpc_test.go
@@ -5,24 +5,22 @@ package grpc
 
 import (
 	"context"
-	"os"
-	"path/filepath"
 	"testing"
 	"time"
 
-	"github.com/ChainSafe/chainlink-cosmos/app"
+	testnet "github.com/ChainSafe/chainlink-cosmos/testutil/network"
 	"github.com/ChainSafe/chainlink-cosmos/x/chainlink/types"
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/config"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
-	xauthsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
-	xauthtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	"google.golang.org/grpc"
 )
 
@@ -33,36 +31,10 @@ type testAccount struct {
 }
 
 var (
-	acc1 = generateKeyPair()
-	acc2 = generateKeyPair()
-	acc3 = generateKeyPair()
+	alice = generateKeyPair()
+	bob   = generateKeyPair()
+	cerlo = generateKeyPair()
 )
-
-func initClientContext(t testing.TB) context.Context {
-	userHomeDir, err := os.UserHomeDir()
-	require.NoError(t, err)
-	home := filepath.Join(userHomeDir, ".chainlinkd")
-
-	encodingConfig := app.MakeEncodingConfig()
-	clientCtx := client.Context{}.
-		WithHomeDir(home).
-		WithViper("").
-		WithAccountRetriever(xauthtypes.AccountRetriever{}).
-		WithInterfaceRegistry(encodingConfig.InterfaceRegistry)
-
-	clientCtx, err = config.ReadFromClientConfig(clientCtx)
-	require.NoError(t, err)
-
-	keyring, err := client.NewKeyringFromBackend(clientCtx, "test")
-	require.NoError(t, err)
-
-	clientCtx = clientCtx.WithKeyring(keyring)
-
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, client.ClientContextKey, &clientCtx)
-
-	return ctx
-}
 
 func generateKeyPair() *testAccount {
 	priv, pub, addr := testdata.KeyTestPubAddr()
@@ -73,35 +45,130 @@ func generateKeyPair() *testAccount {
 	return &testAccount{priv, addr, cosmosPubKey}
 }
 
-func setupGRPCConn(t testing.TB) *grpc.ClientConn {
-	grpcConn, err := grpc.Dial(
-		"127.0.0.1:9090",
-		grpc.WithInsecure(),
-	)
-	if err != nil {
-		t.Error(err)
-	}
-
-	return grpcConn
+func TestGRPCTestSuite(t *testing.T) {
+	suite.Run(t, new(GRPCTestSuite))
 }
 
-func Sign(t testing.TB, ctx context.Context, name string, txBuilder client.TxBuilder) error {
-	clientCtx := ctx.Value(client.ClientContextKey).(*client.Context)
+type GRPCTestSuite struct {
+	suite.Suite
 
-	key, err := clientCtx.Keyring.Key(name)
-	require.NoError(t, err)
-	pubKey := key.GetPubKey()
+	ctx       sdk.Context
+	clientCtx client.Context
 
-	accNum, accSeq, err := clientCtx.AccountRetriever.GetAccountNumberSequence(*clientCtx, key.GetAddress())
-	require.NoError(t, err)
+	config   testnet.Config
+	network  *testnet.Network
+	grpcConn *grpc.ClientConn
+}
+
+func (s *GRPCTestSuite) SetupTest() {
+	s.T().Log("setup test suite")
+
+	s.config = testnet.DefaultConfig()
+	s.config.NumValidators = 1
+
+	// configure genesis data for auth module
+	var authGenState authtypes.GenesisState
+	s.config.Codec.MustUnmarshalJSON(s.config.GenesisState[authtypes.ModuleName], &authGenState)
+	genAccounts, err := authtypes.UnpackAccounts(authGenState.Accounts)
+	s.Require().NoError(err)
+	genAccounts = append(genAccounts, &authtypes.BaseAccount{
+		Address:       alice.Addr.String(),
+		AccountNumber: 1,
+		Sequence:      0,
+	})
+	genAccounts = append(genAccounts, &authtypes.BaseAccount{
+		Address:       bob.Addr.String(),
+		AccountNumber: 2,
+		Sequence:      0,
+	})
+	genAccounts = append(genAccounts, &authtypes.BaseAccount{
+		Address:       cerlo.Addr.String(),
+		AccountNumber: 3,
+		Sequence:      0,
+	})
+	accounts, err := authtypes.PackAccounts(genAccounts)
+	s.Require().NoError(err)
+	authGenState.Accounts = accounts
+	s.config.GenesisState[authtypes.ModuleName] = s.config.Codec.MustMarshalJSON(&authGenState)
+
+	// configure genesis data for bank module
+	balances := sdk.NewCoins(
+		sdk.NewCoin("link", s.config.AccountTokens),
+		sdk.NewCoin(s.config.BondDenom, s.config.StakingTokens),
+	)
+	var bankGenState banktypes.GenesisState
+	s.config.Codec.MustUnmarshalJSON(s.config.GenesisState[banktypes.ModuleName], &bankGenState)
+	bankGenState.Balances = append(bankGenState.Balances, banktypes.Balance{
+		Address: alice.Addr.String(),
+		Coins:   balances.Sort(),
+	})
+	bankGenState.Balances = append(bankGenState.Balances, banktypes.Balance{
+		Address: bob.Addr.String(),
+		Coins:   balances.Sort(),
+	})
+	bankGenState.Balances = append(bankGenState.Balances, banktypes.Balance{
+		Address: cerlo.Addr.String(),
+		Coins:   balances.Sort(),
+	})
+	s.config.GenesisState[banktypes.ModuleName] = s.config.Codec.MustMarshalJSON(&bankGenState)
+
+	// configure genesis data for chainlink module
+	var chainlinkGenState types.GenesisState
+	chainlinkGenState.ModuleOwners = []*types.MsgModuleOwner{{Address: alice.Addr, PubKey: []byte(alice.Cosmos), AssignerAddress: nil}}
+	s.config.GenesisState[types.ModuleName] = s.config.Codec.MustMarshalJSON(&chainlinkGenState)
+
+	s.network = testnet.New(s.T(), s.config)
+	_, err = s.network.WaitForHeight(1)
+	s.Require().NoError(err)
+
+	s.grpcConn, err = grpc.Dial(
+		s.network.Validators[0].AppConfig.GRPC.Address,
+		grpc.WithInsecure(),
+	)
+	require.NoError(s.T(), err)
+}
+
+func (s *GRPCTestSuite) BroadcastTx(ctx context.Context, submitter *testAccount, msgs ...sdk.Msg) *tx.BroadcastTxResponse {
+	txClient := tx.NewServiceClient(s.grpcConn)
+
+	encCfg := simapp.MakeTestEncodingConfig()
+	txBuilder := encCfg.TxConfig.NewTxBuilder()
+
+	err := txBuilder.SetMsgs(msgs...)
+	s.Require().NoError(err)
+
+	txBuilder.SetGasLimit(testdata.NewTestGasLimit())
+	//txBuilder.SetFeeAmount(...)
+	//txBuilder.SetMemo(...)
+	//txBuilder.SetTimeoutHeight(...)
+
+	s.Require().NoError(s.Sign(submitter, txBuilder))
+
+	txBytes, err := encCfg.TxConfig.TxEncoder()(txBuilder.GetTx())
+	s.Require().NoError(err)
+
+	res, err := txClient.BroadcastTx(ctx, &tx.BroadcastTxRequest{
+		Mode:    tx.BroadcastMode_BROADCAST_MODE_SYNC,
+		TxBytes: txBytes,
+	})
+	s.Require().NoError(err)
+
+	return res
+}
+
+func (s *GRPCTestSuite) Sign(signer *testAccount, txBuilder client.TxBuilder) error {
+	//accNum, accSeq, err := s.clientCtx.AccountRetriever.GetAccountNumberSequence(s.clientCtx, signer.Addr)
+	//s.Require().NoError(err)
+
+	_, accSeq := uint64(1), uint64(0)
 
 	encCfg := simapp.MakeTestEncodingConfig()
 
-	signerData := xauthsigning.SignerData{
-		ChainID:       "testchain",
-		AccountNumber: accNum,
-		Sequence:      accSeq,
-	}
+	//signerData := xauthsigning.SignerData{
+	//	ChainID:       "testchain",
+	//	AccountNumber: accNum,
+	//	Sequence:      accSeq,
+	//}
 
 	signMode := encCfg.TxConfig.SignModeHandler().DefaultMode()
 	sigData := signing.SingleSignatureData{
@@ -109,26 +176,27 @@ func Sign(t testing.TB, ctx context.Context, name string, txBuilder client.TxBui
 		Signature: nil,
 	}
 	sig := signing.SignatureV2{
-		PubKey:   pubKey,
+		PubKey:   signer.Priv.PubKey(),
 		Data:     &sigData,
 		Sequence: accSeq,
 	}
 
-	err = txBuilder.SetSignatures(sig)
-	require.NoError(t, err)
+	err := txBuilder.SetSignatures(sig)
+	s.Require().NoError(err)
 
-	bytesToSign, err := encCfg.TxConfig.SignModeHandler().GetSignBytes(signMode, signerData, txBuilder.GetTx())
-	require.NoError(t, err)
+	//bytesToSign, err := encCfg.TxConfig.SignModeHandler().GetSignBytes(signMode, signerData, txBuilder.GetTx())
+	//s.Require().NoError(err)
 
-	sigBytes, _, err := clientCtx.Keyring.Sign(name, bytesToSign)
-	require.NoError(t, err)
+	//sigBytes, _, err := s.clientCtx.Keyring.Sign("alice", bytesToSign) // FIXME
+	//s.Require().NoError(err)
 
 	sigData = signing.SingleSignatureData{
-		SignMode:  signMode,
-		Signature: sigBytes,
+		SignMode: signMode,
+		//Signature: sigBytes,
+		Signature: nil,
 	}
 	sig = signing.SignatureV2{
-		PubKey:   pubKey,
+		PubKey:   signer.Priv.PubKey(),
 		Data:     &sigData,
 		Sequence: accSeq,
 	}
@@ -136,122 +204,75 @@ func Sign(t testing.TB, ctx context.Context, name string, txBuilder client.TxBui
 	return txBuilder.SetSignatures(sig)
 }
 
-func BroadcastTx(t testing.TB, ctx context.Context, grpcConn *grpc.ClientConn, submitter string, msgs ...sdk.Msg) *tx.BroadcastTxResponse {
-	//clientCtx := ctx.Value(client.ClientContextKey).(*client.Context)
-	txClient := tx.NewServiceClient(grpcConn)
-
-	encCfg := simapp.MakeTestEncodingConfig()
-	txBuilder := encCfg.TxConfig.NewTxBuilder()
-
-	err := txBuilder.SetMsgs(msgs...)
-	require.NoError(t, err)
-
-	txBuilder.SetGasLimit(testdata.NewTestGasLimit())
-	//txBuilder.SetFeeAmount(...)
-	//txBuilder.SetMemo(...)
-	//txBuilder.SetTimeoutHeight(...)
-
-	require.NoError(t, Sign(t, ctx, submitter, txBuilder))
-
-	txBytes, err := encCfg.TxConfig.TxEncoder()(txBuilder.GetTx())
-	require.NoError(t, err)
-
-	res, err := txClient.BroadcastTx(ctx, &tx.BroadcastTxRequest{
-		Mode:    tx.BroadcastMode_BROADCAST_MODE_SYNC,
-		TxBytes: txBytes,
-	})
-	require.NoError(t, err)
-
-	return res
-}
-
-func TestGRPC(t *testing.T) {
-	ctx := initClientContext(t)
-	grpcConn := setupGRPCConn(t)
-	queryClient := types.NewQueryClient(grpcConn)
-
-	clientCtx := ctx.Value(client.ClientContextKey).(*client.Context)
-
-	alice, err := clientCtx.Keyring.Key("alice")
-	require.NoError(t, err)
-	aliceCosmosPubKey, err := sdk.Bech32ifyPubKey(sdk.Bech32PubKeyTypeAccPub, alice.GetPubKey())
-	require.NoError(t, err)
-
-	bob, err := clientCtx.Keyring.Key("bob")
-	require.NoError(t, err)
-	bobCosmosPubKey, err := sdk.Bech32ifyPubKey(sdk.Bech32PubKeyTypeAccPub, bob.GetPubKey())
-	require.NoError(t, err)
-
-	cerlo, err := clientCtx.Keyring.Key("cerlo")
-	require.NoError(t, err)
-	cerloCosmosPubKey, err := sdk.Bech32ifyPubKey(sdk.Bech32PubKeyTypeAccPub, cerlo.GetPubKey())
-	require.NoError(t, err)
+func (s *GRPCTestSuite) TestIntegration() {
+	ctx := context.Background()
+	queryClient := types.NewQueryClient(s.grpcConn)
 
 	// 1 - Check initial module owner
 	getModuleOwnerResponse, err := queryClient.GetAllModuleOwner(ctx, &types.GetModuleOwnerRequest{})
-	require.NoError(t, err)
+	s.Require().NoError(err)
 	moduleOwner := getModuleOwnerResponse.GetModuleOwner()
-	require.Equal(t, 1, len(moduleOwner))
-	require.Equal(t, alice.GetAddress(), moduleOwner[0].GetAddress())
-	require.Equal(t, aliceCosmosPubKey, string(moduleOwner[0].GetPubKey()))
+	s.Require().Equal(1, len(moduleOwner))
+	s.Require().Equal(alice.Addr, moduleOwner[0].GetAddress())
+	s.Require().Equal(alice.Cosmos, string(moduleOwner[0].GetPubKey()))
 
 	// 2 - Add new module owner by alice
 	addModuleOwnerTx := &types.MsgModuleOwner{
-		Address:         bob.GetAddress(),
-		PubKey:          []byte(bobCosmosPubKey),
-		AssignerAddress: alice.GetAddress(),
+		Address:         bob.Addr,
+		PubKey:          []byte(bob.Cosmos),
+		AssignerAddress: alice.Addr,
 	}
-	require.NoError(t, addModuleOwnerTx.ValidateBasic())
-	addModuleOwnerTxResponse := BroadcastTx(t, ctx, grpcConn, alice.GetName(), addModuleOwnerTx)
-	require.EqualValues(t, 0, addModuleOwnerTxResponse.TxResponse.Code)
+	s.Require().NoError(addModuleOwnerTx.ValidateBasic())
+	addModuleOwnerTxResponse := s.BroadcastTx(ctx, alice, addModuleOwnerTx)
+	s.Require().EqualValues(0, addModuleOwnerTxResponse.TxResponse.Code)
 
 	time.Sleep(5 * time.Second)
 
 	getModuleOwnerResponse, err = queryClient.GetAllModuleOwner(ctx, &types.GetModuleOwnerRequest{})
-	require.NoError(t, err)
+	s.Require().NoError(err)
 	moduleOwner = getModuleOwnerResponse.GetModuleOwner()
-	require.Equal(t, 2, len(moduleOwner))
+	s.Require().Equal(2, len(moduleOwner))
 
 	// 3 - Module ownership transfer by bob to alice
 	moduleOwnershipTransferTx := &types.MsgModuleOwnershipTransfer{
-		NewModuleOwnerAddress: alice.GetAddress(),
-		NewModuleOwnerPubKey:  []byte(aliceCosmosPubKey),
-		AssignerAddress:       bob.GetAddress(),
+		NewModuleOwnerAddress: alice.Addr,
+		NewModuleOwnerPubKey:  []byte(alice.Cosmos),
+		AssignerAddress:       bob.Addr,
 	}
-	require.NoError(t, addModuleOwnerTx.ValidateBasic())
-	moduleOwnershipTransferTxResponse := BroadcastTx(t, ctx, grpcConn, bob.GetName(), moduleOwnershipTransferTx)
-	require.EqualValues(t, 0, moduleOwnershipTransferTxResponse.TxResponse.Code)
+	s.Require().NoError(addModuleOwnerTx.ValidateBasic())
+	moduleOwnershipTransferTxResponse := s.BroadcastTx(ctx, bob, moduleOwnershipTransferTx)
+	s.Require().EqualValues(0, moduleOwnershipTransferTxResponse.TxResponse.Code)
 
 	// 4 - Add new feed by alice
 	feedId := "testfeed1"
 	addFeedTx := &types.MsgFeed{
 		FeedId:    feedId,
-		FeedOwner: cerlo.GetAddress(),
+		FeedOwner: cerlo.Addr,
 		DataProviders: []*types.DataProvider{
 			{
-				Address: cerlo.GetAddress(),
-				PubKey:  []byte(cerloCosmosPubKey),
+				Address: cerlo.Addr,
+				PubKey:  []byte(cerlo.Cosmos),
 			},
 		},
 		SubmissionCount:           1,
 		HeartbeatTrigger:          2,
 		DeviationThresholdTrigger: 3,
 		FeedReward:                4,
-		ModuleOwnerAddress:        alice.GetAddress(),
+		ModuleOwnerAddress:        alice.Addr,
 	}
-	require.NoError(t, addFeedTx.ValidateBasic())
-	addFeedTxResponse := BroadcastTx(t, ctx, grpcConn, alice.GetName(), addFeedTx)
-	require.EqualValues(t, 0, addFeedTxResponse.TxResponse.Code)
+	s.Require().NoError(addFeedTx.ValidateBasic())
+	addFeedTxResponse := s.BroadcastTx(ctx, alice, addFeedTx)
+	s.Require().EqualValues(0, addFeedTxResponse.TxResponse.Code)
 
 	time.Sleep(5 * time.Second)
 
 	getFeedByFeedIdResponse, err := queryClient.GetFeedByFeedId(ctx, &types.GetFeedByIdRequest{FeedId: feedId})
-	require.NoError(t, err)
+	s.Require().NoError(err)
 	feed := getFeedByFeedIdResponse.GetFeed()
 
-	require.Equal(t, feedId, feed.GetFeedId())
-	require.EqualValues(t, 1, feed.GetSubmissionCount())
-	require.EqualValues(t, 2, feed.GetHeartbeatTrigger())
-	require.EqualValues(t, 3, feed.GetDeviationThresholdTrigger())
-	require.EqualValues(t, 4, feed.GetFeedReward())
+	s.Require().Equal(feedId, feed.GetFeedId())
+	s.Require().EqualValues(1, feed.GetSubmissionCount())
+	s.Require().EqualValues(2, feed.GetHeartbeatTrigger())
+	s.Require().EqualValues(3, feed.GetDeviationThresholdTrigger())
+	s.Require().EqualValues(4, feed.GetFeedReward())
 }
