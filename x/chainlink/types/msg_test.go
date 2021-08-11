6a734521a0f4fda9c79814ecf05356008db1ad48
araskachoi <choidanielw@gmail.com>
2021-08-11 14:05:03 -0700

    WIP: pubkey wrong?

--- x/chainlink/types/msg_test.go
+++ x/chainlink/types/msg_test.go
@@ -4,6 +4,7 @@
 package types
 
 import (
+	fmt "fmt"
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/crypto/types"
@@ -12,18 +13,18 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+// TODO: replace this method and import the one from util_test.go after merged.
+func GenerateAccount() (types.PrivKey, types.PubKey, sdk.AccAddress) {
+	priv, pub, addr := testdata.KeyTestPubAddr()
+	return priv, pub, addr
+}
+
 type MsgFeedDataTestSuite struct {
 	suite.Suite
 	submitter  sdk.AccAddress
 	feedId     string
 	feedData   []byte
 	signatures [][]byte
-}
-
-// TODO: replace this method and import the one from util_test.go after merged.
-func GenerateAccount() (types.PrivKey, types.PubKey, sdk.AccAddress) {
-	priv, pub, addr := testdata.KeyTestPubAddr()
-	return priv, pub, addr
 }
 
 func TestMsgFeedDataTestSuite(t *testing.T) {
@@ -63,12 +64,12 @@ func (ts *MsgFeedDataTestSuite) TestMsgFeedDataValidateBasic() {
 		signatures  [][]byte
 		expPass     bool
 	}{
-		{description: "passing test - all valid values", submitter: ts.submitter, feedId: ts.feedId, feedData: ts.feedData, signatures: ts.signatures, expPass: true},
-		{description: "failing test - empty submitter", submitter: nil, feedId: ts.feedId, feedData: ts.feedData, signatures: ts.signatures, expPass: false},
-		{description: "failing test - empty feedId", submitter: ts.submitter, feedId: "", feedData: ts.feedData, signatures: ts.signatures, expPass: false},
-		{description: "failing test - invalid feedId format", submitter: ts.submitter, feedId: "BAD/FEED/ID", feedData: ts.feedData, signatures: ts.signatures, expPass: false},
-		{description: "failing test - empty feedData", submitter: ts.submitter, feedId: ts.feedId, feedData: nil, signatures: ts.signatures, expPass: false},
-		{description: "failing test - empty signatures", submitter: ts.submitter, feedId: ts.feedId, feedData: ts.feedData, signatures: [][]byte{}, expPass: false},
+		{description: "MsgFeedDataTestSuite: passing case - all valid values", submitter: ts.submitter, feedId: ts.feedId, feedData: ts.feedData, signatures: ts.signatures, expPass: true},
+		{description: "MsgFeedDataTestSuite: failing case - empty submitter", submitter: nil, feedId: ts.feedId, feedData: ts.feedData, signatures: ts.signatures, expPass: false},
+		{description: "MsgFeedDataTestSuite: failing case - empty feedId", submitter: ts.submitter, feedId: "", feedData: ts.feedData, signatures: ts.signatures, expPass: false},
+		{description: "MsgFeedDataTestSuite: failing case - invalid feedId format", submitter: ts.submitter, feedId: "BAD/FEED/ID", feedData: ts.feedData, signatures: ts.signatures, expPass: false},
+		{description: "MsgFeedDataTestSuite: failing case - empty feedData", submitter: ts.submitter, feedId: ts.feedId, feedData: nil, signatures: ts.signatures, expPass: false},
+		{description: "MsgFeedDataTestSuite: failing case - empty signatures", submitter: ts.submitter, feedId: ts.feedId, feedData: ts.feedData, signatures: [][]byte{}, expPass: false},
 	}
 
 	for i, tc := range testCases {
@@ -90,15 +91,81 @@ func (ts *MsgFeedDataTestSuite) TestMsgFeedDataValidateBasic() {
 
 type MsgModuleOwnerTestSuite struct {
 	suite.Suite
-	assigner  sdk.AccAddress
-	address   sdk.AccAddress
-	publicKey []byte
+	assignerAddress          sdk.AccAddress
+	assignerPublicKey        []byte
+	newModOwnerAddress       sdk.AccAddress
+	newModOwnerPublicKey     []byte
+	invalidModOwnerAddress   sdk.AccAddress
+	invalidModOwnerPublicKey []byte
+}
+
+func TestMsgModuleOwnerTestSuite(t *testing.T) {
+	suite.Run(t, new(MsgModuleOwnerTestSuite))
 }
 
 func (ts *MsgModuleOwnerTestSuite) SetupTest() {
 	// assigner is a different account than the address + publicKey
-	_, _, ts.assigner = GenerateAccount()
 	_, pubkey, addr := GenerateAccount()
-	ts.publicKey = pubkey.Bytes()
-	ts.address = addr
+	ts.assignerAddress = addr
+	ts.assignerPublicKey = pubkey.Bytes()
+
+	_, pubkey, addr = GenerateAccount()
+	ts.newModOwnerAddress = addr
+	ts.newModOwnerPublicKey = pubkey.Bytes()
+
+	_, pubkey, addr = GenerateAccount()
+	ts.invalidModOwnerAddress = addr
+	ts.invalidModOwnerPublicKey = pubkey.Bytes()
+}
+
+func (ts *MsgModuleOwnerTestSuite) TestMsgModuleOwnerConstructor() {
+	msg := NewMsgModuleOwner(
+		ts.assignerAddress,
+		ts.newModOwnerAddress,
+		ts.newModOwnerPublicKey,
+	)
+
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	signedBytes := sdk.MustSortJSON(bz)
+
+	ts.Require().Equal(msg.Route(), RouterKey)
+	ts.Require().Equal(msg.Type(), AddModuleOwner)
+	ts.Require().Equal(msg.GetSigners(), []sdk.AccAddress{ts.assignerAddress})
+	ts.Require().Equal(msg.GetSignBytes(), signedBytes)
+}
+
+func (ts *MsgModuleOwnerTestSuite) TestMsgModuleOwnerValidateBasic() {
+	testCases := []struct {
+		description string
+		assigner    sdk.AccAddress
+		address     sdk.AccAddress
+		publicKey   []byte
+		expPass     bool
+	}{
+		{description: "MsgModuleOwnerTestSuite: passing case - all valid values", assigner: ts.assignerAddress, address: ts.newModOwnerAddress, publicKey: ts.newModOwnerPublicKey, expPass: true},
+		{description: "MsgModuleOwnerTestSuite: failing case - address and publicKey does not match", assigner: ts.assignerAddress, address: ts.newModOwnerAddress, publicKey: ts.invalidModOwnerPublicKey, expPass: false},
+	}
+
+	for i, tc := range testCases {
+		msg := NewMsgModuleOwner(
+			tc.assigner,
+			tc.address,
+			tc.publicKey,
+		)
+		fmt.Println(ts.assignerAddress)
+		fmt.Println((ts.assignerPublicKey))
+
+		fmt.Println(ts.newModOwnerAddress)
+		fmt.Println((ts.newModOwnerPublicKey))
+
+		fmt.Println(ts.invalidModOwnerAddress)
+		fmt.Println((ts.invalidModOwnerPublicKey))
+		err := msg.ValidateBasic()
+
+		if tc.expPass {
+			ts.Require().NoError(err, "valid test %d failed: %s, %v", i, tc.description)
+		} else {
+			ts.Require().Error(err, "invalid test %d passed: %s, %v", i, tc.description)
+		}
+	}
 }
