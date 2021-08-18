296ea79227fd858dffb2a7ecab8749e492395d76
araskachoi <choidanielw@gmail.com>
2021-08-18 14:49:54 -0700

    update test cases

--- scripts/testAccount.sh
+++ scripts/testAccount.sh
@@ -38,32 +38,46 @@ echo "getting alice chainlink account information"
 getAliceAccountInfo=$(chainlinkd query chainlink getAccountInfo $(chainlinkd keys show alice -a) --from alice --keyring-backend test --chain-id testchain)
 
 aliceSubmitterAddress=$(echo "$getAliceAccountInfo" | jq '.account.submitter')
-echo "$aliceSubmitterAddress"
-echo "$aliceAddr"
 if [ "$aliceSubmitterAddress" != "\"$aliceAddr\"" ]
 then
   errorAndExit "Error incorrect account submitter address, expecting: $aliceAddr, got: $aliceSubmitterAddress"
 fi
  
 aliceChainlinkPublicKey=$(echo "$getAliceAccountInfo" | jq '.account.chainlinkPublicKey')
-echo $aliceChainlinkPublicKey
 if [ "$aliceChainlinkPublicKey" != "\"YWxpY2VDaGFpbmxpbmtQdWJLZXk=\"" ]
 then
   errorAndExit "Error incorrect account public key, expecting: YWxpY2VDaGFpbmxpbmtQdWJLZXk=, got: $aliceChainlinkPublicKey"
 fi
 
 aliceChainlinkSigningKey=$(echo "$getAliceAccountInfo" | jq '.account.chainlinkSigningKey')
-echo $aliceChainlinkSigningKey
 if [ "$aliceChainlinkSigningKey" != "\"YWxpY2VDaGFpbmxpbmtTaWduaW5nS2V5\"" ]
 then
   errorAndExit "Error incorrect account signing key, expecting: YWxpY2VDaGFpbmxpbmtTaWduaW5nS2V5, got: $aliceChainlinkSigningKey"
 fi
 
 alicePiggyAddress=$(echo "$getAliceAccountInfo" | jq '.account.piggyAddress')
-echo $alicePiggyAddress
 if [ "$alicePiggyAddress" != "\"$aliceAddr\"" ]
 then
   errorAndExit "Error incorrect account piggy address, expecting: $aliceAddr, got: $alicePiggyAddress"
+fi
+
+# mAkE sUrE bOb AcCoUnT dOeS nOt ExIsT
+echo "getting bob chainlink account information"
+getBobAccountInfo=$(chainlinkd query chainlink getAccountInfo $(chainlinkd keys show bob -a) --from bob --keyring-backend test --chain-id testchain)
+getBobAccountInfoResp=$(echo ${getBobAccountInfo#*\]} | jq '.account.piggyAddress')
+if [ "$getBobAccountInfoResp" != "" ]
+then
+  errorAndExit "Error getting bob's chainlink account: $getBobAccountInfo"
+fi
+
+# dIsAlLoW rEpEaT aCcOuNt CrEaTiOn
+echo "attempting to add alice chainlink account again"
+addChainlinkAccountTx=$(chainlinkd tx chainlink add-chainlink-account "aliceChainlinkPubKey" "aliceChainlinkSigningKey" --from alice --keyring-backend test --chain-id testchain <<< 'y\n')
+sleep 1
+addChainlinkAccountTxResp=$(echo ${addChainlinkAccountTx#*\]} | jq '.height')
+if [ "$addChainlinkAccountTxResp" != "\"0\"" ]
+then
+  errorAndExit "Error in rejecting alice's chainlink account: $addChainlinkAccountTx"
 fi
 
 pkill chainlinkd
