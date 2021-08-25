65b102454fbfa93c488a878fb4de1e79b1dd6a85
araskachoi <choidanielw@gmail.com>
2021-08-25 15:33:14 -0700

    add tests

--- x/chainlink/types/coin_test.go
+++ x/chainlink/types/coin_test.go
@@ -30,5 +30,50 @@ func TestTypes_NewLinkCoin(t *testing.T) {
 	require.Equal(t, hundredLink.Add(hundredLink), NewLinkCoin(sdk.NewInt(200)))
 
 	require.Equal(t, hundredLink.Sub(zeroLink), hundredLink)
+	require.Equal(t, NewLinkCoin(sdk.NewInt(200)).Sub(hundredLink), hundredLink)
+}
 
+func TestTypes_NewLinkDecCoin(t *testing.T) {
+	hundredAmount := sdk.NewInt(100)
+	hundredDecLink := NewLinkDecCoin(hundredAmount)
+	require.NoError(t, hundredDecLink.Validate())
+	require.True(t, hundredDecLink.IsValid())
+
+	zeroAmount := sdk.NewInt(0)
+	zeroDecLink := NewLinkDecCoin(zeroAmount)
+	require.True(t, zeroDecLink.IsZero())
+	require.True(t, zeroDecLink.IsValid())
+	require.NoError(t, zeroDecLink.Validate())
+
+	require.Equal(t, hundredDecLink.GetDenom(), LinkDenom)
+
+	require.Equal(t, hundredDecLink.Amount, sdk.NewDec(hundredAmount.Int64()))
+	require.Equal(t, zeroDecLink.Amount, sdk.NewDec(zeroAmount.Int64()))
+
+	require.Equal(t, hundredDecLink.Add(zeroDecLink), hundredDecLink)
+	require.Equal(t, hundredDecLink.Add(hundredDecLink), NewLinkDecCoin(sdk.NewInt(200)))
+
+	require.Equal(t, hundredDecLink.Sub(zeroDecLink), hundredDecLink)
+	require.Equal(t, NewLinkDecCoin(sdk.NewInt(200)).Sub(hundredDecLink), hundredDecLink)
+
+	require.True(t, hundredDecLink.IsGTE(zeroDecLink))
+	require.True(t, zeroDecLink.IsLT(hundredDecLink))
+
+	require.True(t, hundredDecLink.IsEqual(NewLinkDecCoin(sdk.NewInt(100))))
+	require.True(t, zeroDecLink.IsEqual(NewLinkDecCoin(sdk.NewInt(0))))
+}
+
+func TestTypes_NewLinkCoin64(t *testing.T) {
+	hundredAmount := int64(100)
+	hundredLink := NewLinkCoinInt64(hundredAmount)
+	require.NoError(t, hundredLink.Validate())
+	require.True(t, hundredLink.IsValid())
+	require.Equal(t, hundredLink, NewLinkCoin(sdk.NewInt(100)))
+
+	zeroAmount := int64(0)
+	zeroLink := NewLinkCoinInt64(zeroAmount)
+	require.True(t, zeroLink.IsZero())
+	require.True(t, zeroLink.IsValid())
+	require.NoError(t, zeroLink.Validate())
+	require.Equal(t, zeroLink, NewLinkCoin(sdk.NewInt(0)))
 }
